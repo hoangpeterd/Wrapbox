502954bb4acb7715e33ca1bea5a541d8c2a16955
Peter Hoàng <peterhoang@utexas.edu>
2017-02-13 17:51:16 -0600

    Express ready, modules added to .gitignore

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// dependencies
 var express = require('express');
 var app = express();
 var path = require('path');
 
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
-})
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(8080);
