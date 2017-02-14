48555cd53a05be522ac85722851ba326d4af6490
Peter Hoàng <peterhoang@utexas.edu>
2017-02-13 18:55:07 -0600

    Switched over to Node

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/../public/index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../public')));
 app.listen(8080);
