6d571f100863da378dc1e7761e90a4ffc5c2809d
Peter Hoàng <peterhoang@utexas.edu>
2017-02-16 03:40:58 -0600

    Added modals in correlation to the sign in button

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
+
   $("#fullpage").fullpage({
     anchors: ["page1", "page2", "page3", "page4"],
     menu: "#menu"
   });
+
+  //when clicked on the signin button a modal will show up
+  $("#modalSignIn").on("click",function() {
+    $("#signInModal").modal();
+  });
+
+  //when clicked on the signup button a modal will show up
+  $("#modalSignUp").on("click", function() {
+    $("#signUpModal").modal();
+  });
+
 });
